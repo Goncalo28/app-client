b725de1bc7edaf24f178ce4d5dc50f9001d1bad3
Luis Brazao <luis.ms.brazao@gmail.com>
2020-12-02 17:25:33 +0000

    auth api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import React from "react"
+import { Redirect, Route, Switch } from "react-router-dom";
+import AuthService from "./utils/auth";
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
-function App() {
-  return (
-    <div>
-      Hello
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    loggedInUser: null,
+  };
+
+  setCurrentUser = (user) => {
+    this.setState({
+      loggedInUser: user,
+    });
+  };
+
+  componentDidMount() {
+    if (this.state.loggedInUser === null) {
+      const authService = new AuthService();
+      authService.loggedin().then((response) => {
+        if (response.data._id) {
+          this.setCurrentUser(response.data);
+        } else {
+          localStorage.removeItem("loggedInUser");
+        }
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <ToastContainer />
+        {/* <Navbar
+          loggedInUser={this.state.loggedInUser}
+          setCurrentUser={this.setCurrentUser}
+        /> */}
+        <Switch>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
+
